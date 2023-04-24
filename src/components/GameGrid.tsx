92cc03b99e8bfef241672dd88480eb63ed7b9ac0
Davor Mićić <davor.micic@span.eu>
2023-04-24 13:18:00 +0200

    separated game fetching to a hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,41 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { AxiosError, CanceledError } from "axios";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    apiClient
-      .get<FetchGameResponse>("/games", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError((err as AxiosError).message);
-      });
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
